8f80f801b96105447cc43af59c4d71a8a1436e89
kentaro10 <paddle101733@gmail.com>
2020-11-30 15:34:16 +0900

    ユーザー管理機能実装、postモデル生成

---
+++ db/migrate/20201130061951_create_posts.rb
@@ -0,0 +1,16 @@
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.string  :title,       null: false
+      t.integer :genre1_id,   null: false
+      t.integer :genre2_id,   null: false
+      t.string  :shop_name
+      t.text    :explanation
+      t.integer :self_assessment_id, null: false
+      t.references :user,   foreign_key: true
+
+
+      t.timestamps
+    end
+  end
+end
