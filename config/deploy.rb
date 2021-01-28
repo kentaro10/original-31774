90cefd28b700261c0c5e254f03cbf45d9463f00a
kentaro10 <paddle101733@gmail.com>
2021-01-28 16:52:04 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.15.0'
 set :application, 'original-31774'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com/kentaro10/original-31774.git'
+set :repo_url,  'git@github.com:kentaro10/original-31774.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
